74de55215b760efe63618b8efd57b5f74005c021
jonah1217 <spencjon@oregonstate.edu>
2018-04-29 17:16:58 -0700

    debug statements

--- MinimaxPlayer.cpp
+++ MinimaxPlayer.cpp
@@ -76,10 +76,11 @@ void MinimaxPlayer::get_move(OthelloBoard* b, int& col, int& row) {
 			bestMove  = x;
 		}
 	}
-
+	std::cout <<
 	col = get<0>(bestMove);
 	row = get<1>(bestMove);
-
+	std::cout << "Col: " << col;
+	std::cout << "row: " << row;
 	return; 
 }
 
