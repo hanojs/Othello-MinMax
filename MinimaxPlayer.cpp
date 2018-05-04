ebb34c8d0f50e65026c6d55c8a08641d580e050f
jonah1217 <spencjon@oregonstate.edu>
2018-05-04 16:41:17 -0700

    Reversed cleaning

--- MinimaxPlayer.cpp
+++ MinimaxPlayer.cpp
@@ -34,12 +34,97 @@ MinimaxPlayer::~MinimaxPlayer() {
 }
 
 
+int MinimaxPlayer::min_value(OthelloBoard* currentBoard, char player){
+	if(!currentBoard->has_legal_moves_remaining(player)){
+		return utility(currentBoard, player);	
+	}
+	int minValue = std::numeric_limits<int>::max();
+
+	for( auto x : this->getSuccessorStates(currentBoard, player)){ //go through all possible moves after this
+		minValue = min(minValue, this->max_value(&x));
+	}
+
+	return minValue;
+}
+
+//returns the 
+int MinimaxPlayer::max_value(OthelloBoard* currentBoard){
+	char opponent;
+	if (this->get_symbol() == 'X') {
+		opponent = 'O';
+	}else {
+		opponent = 'X';
+	}
+
+	if(!currentBoard->has_legal_moves_remaining(this->get_symbol() )){
+		return utility(currentBoard, this->get_symbol() );	
+	}
+	int maxValue = std::numeric_limits<int>::lowest();
+
+	for( auto x : this->getSuccessorStates(currentBoard, this->get_symbol() )){  //go through all possible moves after this
+		maxValue = max(maxValue, this->min_value(&x, opponent));
+	}
+
+	return maxValue;
+} 
+
+void MinimaxPlayer::get_move(OthelloBoard* b, int& col, int& row) {
+
+	//Pretty much max function that keeps track of the first-level of moves so we can pass those back
+
+	OthelloBoard currentBoard((OthelloBoard) *b);
+	int bestScore = std::numeric_limits<int>::lowest();
+	int currentScore;
+	move bestMove;
+	char opponent;
+	if (this->get_symbol()  == 'X') {
+		opponent = 'O';
+	}else {
+		opponent = 'X';
+	}
+	//initial maximize function -- keeps track of the best move
+	for( auto x : b->get_possible_moves(this->get_symbol())){
+		currentBoard = *b;
+		currentScore = this->min_value(&currentBoard, opponent);
+		if(currentScore > bestScore){
+			bestScore = currentScore;
+			bestMove  = x;
+		}
+	}
+	col = get<0>(bestMove);
+	row = get<1>(bestMove);
+	currentBoard.play_move(col, row, this->get_symbol());
+	return; 
+}
 
 
-// Utility function that returns the goodness of a terminal node for the minimax player. 
-int MinimaxPlayer::utility(OthelloBoard* b) {
+
+
+
+//Gets all possible states within one move of the current board
+vector<OthelloBoard> MinimaxPlayer::getSuccessorStates(OthelloBoard* currentBoard, char player){
+	
+	vector<OthelloBoard> successorStates;
+
+	for(auto x : currentBoard->get_possible_moves(player)){ //Iterate through all the moves possible
+
+		OthelloBoard tempBoard(*currentBoard);
+		tempBoard.play_move(get<0>(x), get<1>(x), player); 
+		successorStates.push_back (tempBoard);
+
+	}
+	return successorStates;
+}
+
+
+/***********************************
+ * Utility function
+ * Returns the goodness of a terminal node for the minimax player. 
+ * ********************************/
+int MinimaxPlayer::utility(OthelloBoard* b, char player) {
 
 	char opponent;
+
 	if (this->get_symbol() == 'X') {
 		opponent = 'O';
 	}else {
@@ -49,90 +134,6 @@ int MinimaxPlayer::utility(OthelloBoard* b) {
 	return (b->count_score(this->get_symbol()) - b->count_score(opponent));
 
 } 
-
-
-//Chooses a move and plays that
-void MinimaxPlayer::get_move(OthelloBoard* b, int& col, int& row) {
-
-	//Pretty much max function that keeps track of the first-level of moves so we can pass those back
-
-	OthelloBoard currentBoard((OthelloBoard) *b);
-	int bestScore = std::numeric_limits<int>::lowest();
-	int currentScore;
-	move bestMove;
-
-
-	//initial maximize function -- keeps track of the best move, and goes into the minimax functions
-	for( auto x : b->get_possible_moves(this->get_symbol())){
-		currentBoard = *b;
-		currentScore = this->min_value(&currentBoard);
-		if(currentScore > bestScore){
-			bestScore = currentScore;
-			bestMove  = x;
-		}
-	}
-
-	currentBoard.play_move(get<0>(bestMove), get<1>(bestMove), this->get_symbol());
-	return; 
-}
-
-//Min_value utility function for minimax (get_move)
-int MinimaxPlayer::min_value(OthelloBoard* currentBoard){
-	
-	//gets the opponent's symbol
-	char opponent;
-	if (this->get_symbol() == 'X') {
-		opponent = 'O';
-	}else {
-		opponent = 'X';
-	}
-	
-	if(!currentBoard->has_legal_moves_remaining(opponent)){
-		return utility(currentBoard);	
-	}
-	int minValue = std::numeric_limits<int>::max();
-
-	//go through all possible moves after this
-	for( auto x : this->getSuccessorStates(currentBoard, opponent)){ 
-		minValue = min(minValue, this->max_value(&x));
-	}
-
-	return minValue;
-}
-
-//Max_value utility function for minimax (get_move)
-int MinimaxPlayer::max_value(OthelloBoard* currentBoard){
-
-	if(!currentBoard->has_legal_moves_remaining(this->get_symbol() )){
-		return utility(currentBoard);	
-	}
-	int maxValue = std::numeric_limits<int>::lowest();
-
-	//go through all possible moves after this
-	for( auto x : this->getSuccessorStates(currentBoard, this->get_symbol() )){  
-		maxValue = max(maxValue, this->min_value(&x));
-	}
-
-	return maxValue;
-} 
-
-
-
-
-//Returns all possible game states from all possible moves (1 deep) 
-vector<OthelloBoard> MinimaxPlayer::getSuccessorStates(OthelloBoard* currentBoard, char player){
-	
-	vector<OthelloBoard> successorStates;
-
-	//Iterate through all the moves possible
-	for(auto x : currentBoard->get_possible_moves(player)){ 
-		OthelloBoard tempBoard(*currentBoard);
-		tempBoard.play_move(get<0>(x), get<1>(x), player); 
-		successorStates.push_back(tempBoard);
-	}
-	//returns all possible game states
-	return successorStates; 
-}
 
 
 
