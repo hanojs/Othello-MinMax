1b409440d5da5596efdfc9ffc82c1eea56dae494
jonah1217 <spencjon@oregonstate.edu>
2018-05-04 16:12:46 -0700

    Updated name of utility function

--- MinimaxPlayer.h
+++ MinimaxPlayer.h
@@ -32,7 +32,7 @@ public:
 	virtual ~MinimaxPlayer();
 	int max_value(OthelloBoard* currentBoard);
 	int min_value(OthelloBoard* currentBoard, char player);
-	int getGoodness(OthelloBoard* b, char player);
+	int utility(OthelloBoard* b, char player);
 
 
 	vector<OthelloBoard> getSuccessorStates(OthelloBoard* currentBoard, char player);
